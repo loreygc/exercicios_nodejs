9bc4b5e7b354bc87ada73b0c0f5bc58a6639b1dd
Lorena <loosgc@gmail.com>
2020-06-19 20:32:03 -0300

    atividade 3 realizada

--- atividade_3/index.js
+++ atividade_3/index.js
@@ -1,5 +1,10 @@
 // ## Importe a biblioteca ##
 
-
+let rs=require('readline-sync')
 
 // ## Faça o código ##
+
+let areadoquadrado = rs.question ("Digite a área do quadrado ")
+
+let areacalculada = areadoquadrado*areadoquadrado
+    console.log('A área do seu quadrado baseada no valor inserido é '+ areacalculada)
