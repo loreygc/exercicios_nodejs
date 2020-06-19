2b463d96b5baca729b2e08edf03e7f1eab221eee
Lorena <loosgc@gmail.com>
2020-06-19 20:22:41 -0300

    atividade 2 realizada

--- atividade_2/index.js
+++ atividade_2/index.js
@@ -1,5 +1,15 @@
 // ## Importe a biblioteca ##
-
-
-
+let rs= require('readline-sync')
 // ## Faça o código ##
+let positive = "Ótimo, continue protegendo as pessoas que você ama!"
+let negative = "Ei! Você precisa utilizar máscara para sair de casa, assim protegerá as pessoas que ama e a si mesmo!"
+
+let perguntaUsuario = rs.question('Você está utilizando máscara para sair? Digite 1 para sim e 2 para não :)');
+
+
+
+if (perguntaUsuario == 1) {
+    console.log(positive);
+} else if (perguntaUsuario == 2) {
+    console.log(negative);
+}
