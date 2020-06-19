5c93b26c4897f7a4d9e6a8bcc7d4e26dc602007c
Lorena <loosgc@gmail.com>
2020-06-19 19:45:55 -0300

    realização da atividade 1

--- atividade_1/index.js
+++ atividade_1/index.js
@@ -1,5 +1,10 @@
 // ## Importe a biblioteca ##
-
-
-
+let rs= require ("readline-sync")
 // ## Faça o código ##
+let voo = rs.question('Qual o número do seu vôo?')
+let fileira = rs.question('Em qual fileira você quer se sentar?')
+
+console.log('********************')
+console.log('Cartão de embarque para o numero de vôo: ' +voo)
+console.log('Você se sentará na fileira '+fileira)
+console.log('********************')
